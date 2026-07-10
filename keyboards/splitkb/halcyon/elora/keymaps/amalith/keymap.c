1c626a8652542424bcdaaa2ebe86b9ee25600727
Amalith <angeamalith@gmail.com>
2026-07-10 02:34:34 -0700

    Blank out other layers

--- keyboards/splitkb/halcyon/elora/keymaps/amalith/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/amalith/keymap.c
@@ -68,11 +68,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------'                                              `-----------------------------------'
  */
     [_GAME] = LAYOUT_elora_hlc(
-     KC_ESC  , KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,   KC_5 ,                                        KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,  KC_0 , KC_ESC ,
-     KC_TAB  ,KC_QUOTE,KC_COMM,  KC_DOT,   KC_P ,   KC_Y ,                                        KC_F,   KC_G ,  KC_C ,   KC_R ,  KC_L , KC_BSPC,
-     CTL_ESC , KC_A ,  KC_O   ,  KC_E  ,   KC_U ,   KC_I ,                                        KC_D,   KC_H ,  KC_T ,   KC_N ,  KC_S , CTL_MINS,
-     KC_LSFT ,KC_SCLN, KC_Q   ,  KC_J  ,   KC_K ,   KC_X , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_B,   KC_M ,  KC_W ,   KC_V ,  KC_Z , KC_RSFT,
-                                 ADJUST, KC_LGUI, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_SPC ,KC_RALT, KC_RGUI, KC_APP,
+     _______  , _______ ,  _______   ,  _______  ,   _______ ,   _______ ,                                        _______ ,  _______ ,  _______ ,   _______ ,  _______ , _______ ,
+     _______  ,_______,_______,  _______,   _______ ,   _______ ,                                        _______,   _______ ,  _______ ,   _______ ,  _______ , _______,
+     _______ , _______ ,  _______   ,  _______  ,   _______ ,   _______ ,                                        _______,   _______ ,  _______ ,   _______ ,  _______ , _______,
+     _______ ,_______, _______   ,  _______  ,   _______ ,   _______ , _______,_______,     _______  , _______, _______,   _______ ,  _______ ,   _______ ,  _______ , _______,
+                                 _______, _______, _______, _______ , _______   ,     _______    , _______ ,_______, _______, _______,
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 
@@ -96,11 +96,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------'                                              `-----------------------------------'
  */
     [_SYMBOLS] = LAYOUT_elora_hlc(
-     KC_ESC  , KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,   KC_5 ,                                        KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,  KC_0 , KC_ESC ,
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_BSPC,
-     CTL_ESC , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                 ADJUST, KC_LGUI, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_SPC ,KC_RALT, KC_RGUI, KC_APP,
+     _______  , _______ ,  _______   ,  _______  ,   _______ ,   _______ ,                                        _______ ,  _______ ,  _______ ,   _______ ,  _______ , _______ ,
+     _______  , _______ ,  _______   ,  _______  ,   _______ ,   _______ ,                                        _______,   _______ ,  _______ ,   _______ ,_______, _______,
+     _______ , _______ ,  _______   ,  _______  ,   _______ ,   _______ ,                                        _______,   _______ ,  _______ ,   _______ ,  _______ , _______,
+     _______ , _______ ,  _______   ,  _______  ,   _______ ,   _______ , _______,_______,     _______  , _______, _______,   _______ ,_______, _______ ,_______, _______,
+                                 _______, _______, _______, _______ , _______   ,     _______    , _______ ,_______, _______, _______,
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 
