32345bfff7364f0277b8b351dc61f2e662b5f190
Amalith <angeamalith@gmail.com>
2026-07-12 03:57:29 -0700

    Enable hand swap

--- keyboards/splitkb/halcyon/elora/keymaps/amalith/rules.mk
+++ keyboards/splitkb/halcyon/elora/keymaps/amalith/rules.mk
@@ -2,6 +2,7 @@ ENCODER_MAP_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 KEY_LOCK_ENABLE = yes
 REPEAT_KEY_ENABLE = yes
+SWAP_HANDS_ENABLE = yes
 
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
 USER_NAME := halcyon_modules
