2bb2c89af2ff1a7bb9b52f1e24f39320f5646dbb
Amalith <angeamalith@gmail.com>
2026-07-20 18:32:11 -0700

    Fix dynamic macros not functioning
    Looks like for whatever reason using a oneshot key layer switch breaks the macro functionality, and it needs to be a toggled layer.

--- keyboards/splitkb/halcyon/elora/keymaps/amalith/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/amalith/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LCTL(KC_Z) , LCTL(KC_A)   ,  LCTL(KC_I)  ,     KC_UP     ,    LALT(KC_TAB)   ,   KC_F1 ,                                                 _______,   KC_KP_7,  KC_KP_8 ,   KC_KP_9 , LSG(KC_MINUS), KC_END,
         _______ , LCTL(KC_S)   ,  KC_LEFT    ,      KC_DOWN    ,   KC_RGHT    ,   KC_PAUSE ,                                                 KC_MINS,   KC_KP_4 ,  KC_KP_5 ,   KC_KP_6 , KC_BSLS , _______,
      _______    ,  _______  ,  LALT(KC_LSFT)    , LCTL(KC_C)       ,   LCTL(KC_V)    ,   LCTL(KC_F) , _______ , _______ ,   _______  , _______, LSFT(KC_MINS),   KC_KP_1 ,KC_KP_2, KC_KP_3 , KC_KP_0,_______,
-                                            DM_PLY2       ,   _______    ,   _______ , _______ , _______ ,   OSL(_MACRO)  , _______        ,_______, _______, _______,
+                                            DM_PLY2       ,   _______    ,   _______ , _______ , _______ ,   TG(_MACRO)  , _______        ,_______, _______, _______,
      _______, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                _______, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 
