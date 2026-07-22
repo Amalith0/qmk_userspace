8921950d958919149b2a29039663858344e5ca1f
Amalith <angeamalith@gmail.com>
2026-07-22 15:46:42 -0700

    Add a dedicated | key

--- keyboards/splitkb/halcyon/elora/keymaps/amalith/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/amalith/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_SYMBOLS] = LAYOUT_elora_hlc(
         LCTL(KC_Y) , C(S(KC_1))   ,  C(S(KC_2))    ,  C(S(KC_3))  ,   _______    ,   LALT(KC_F4) ,                                                 KC_F6 ,  KC_F7 ,  KC_F8 ,  KC_F9  ,  KC_F10 , KC_HOME ,
-        LCTL(KC_Z) , LCTL(KC_A)   ,  LCTL(KC_I)  ,     KC_UP     ,    LALT(KC_TAB)   ,   KC_F1 ,                                                 _______,   KC_KP_7,  KC_KP_8 ,   KC_KP_9 , LSG(KC_MINUS), KC_END,
+        LCTL(KC_Z) , LCTL(KC_A)   ,  LCTL(KC_I)  ,     KC_UP     ,    LALT(KC_TAB)   ,   KC_F1 ,                                                 LSFT(KC_BSLS),   KC_KP_7,  KC_KP_8 ,   KC_KP_9 , LSG(KC_MINUS), KC_END,
         _______ , LCTL(KC_S)   ,  KC_LEFT    ,      KC_DOWN    ,   KC_RGHT    ,   KC_PAUSE ,                                                 KC_MINS,   KC_KP_4 ,  KC_KP_5 ,   KC_KP_6 , KC_BSLS , _______,
      _______    ,  _______  ,  LALT(KC_LSFT)    , LCTL(KC_C)       ,   LCTL(KC_V)    ,   LCTL(KC_F) , _______ , _______ ,   _______  , _______, LSFT(KC_MINS),   KC_KP_1 ,KC_KP_2, KC_KP_3 , KC_KP_0,_______,
                                             DM_PLY2       ,   _______    ,   _______ , _______ , _______ ,   TG(_MACRO)  , _______        ,_______, _______, _______,
